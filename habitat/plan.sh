459242014fd9154ab8832d9bddfe5565bbc76086
root <root@ossnode4.f1vrntz235aedas3md3gyjdz0b.dx.internal.cloudapp.net>
2017-09-22 05:49:15 +0000

    updated code

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -5,7 +5,7 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/kbhimanavarjula/VotingApp"
 pkg_build_deps=(core/git core/virtualenv)
-pkg_deps=(core/coreutils core/python2 core/redis)
+pkg_deps=(core/coreutils core/python2 core/redis core/make core/gcc)
 pkg_exports=([port]=port)
 pkg_exposes=(port)
 pkg_svc_user="root"
@@ -15,9 +15,9 @@ do_download()
 {
         build_line "do_download() =================================================="
 	wget http://download.redis.io/redis-stable.tar.gz
-	tar xvzf redis-stable.tar.gz
-	cd redis-stable
-#	make install
+	tar xzf redis-stable.tar.gz
+#	cd redis-stable
+#	make
         return 0
 }
 
@@ -48,34 +48,3 @@ do_install() {
         #redis-server install
 }
 
-
-
-
-
-#pkg_source="http://some_source_url/releases/${pkg_name}-${pkg_version}.tar.gz"
-# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-# pkg_shasum="TODO"
-# pkg_deps=(core/glibc)
-# pkg_build_deps=(core/make core/gcc)
-# pkg_lib_dirs=(lib)
-# pkg_include_dirs=(include)
-# pkg_bin_dirs=(bin)
-# pkg_pconfig_dirs=(lib/pconfig)
-# pkg_svc_run="bin/haproxy -f $pkg_svc_config_path/haproxy.conf"
-# pkg_exports=(
-#   [host]=srv.address
-#   [port]=srv.port
-#   [ssl-port]=srv.ssl.port
-# )
-# pkg_exposes=(port ssl-port)
-# pkg_binds=(
-#   [database]="port host"
-# )
-# pkg_binds_optional=(
-#   [storage]="port host"
-# )
-# pkg_interpreters=(bin/bash)
-# pkg_svc_user="hab"
-# pkg_svc_group="$pkg_svc_user"
-# pkg_description="Some description."
-# pkg_upstream_url="http://example.com/project-name"
